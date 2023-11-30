26ee34ee09e11764573fe51e74cfa94225909282
Solano <hsolano@basin.cs.middlebury.edu>
2023-11-30 11:23:42 -0500

    stuff is coming along

--- mysh.c
+++ mysh.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /* We are permitted to assume 4096 as max char num as input */
-#define COMMAND_LIMIT 4096
-#define TOKEN_DELIM " \t\n\0"
+#define INPUT_LIMIT 4096
+#define COMMAND_LIMIT 10 //rlly 11 (cuz its 10 commands + the program to run)
+#define TOKEN_DELIM " \t\n\0" 
 
 char* readInp(void) {
 
 	/* Buffer for shoving our command */
-	int shellInpSiz = COMMAND_LIMIT;
+	int shellInpSiz = INPUT_LIMIT;
 	char* shellInp = malloc(sizeof(char) * shellInpSiz);
 	if(shellInp == NULL) {
 		return NULL;
 	}
 	
-	/* Where we listen for input indefinitely until either the user does something */
+	/* Where we listen for input indefinitely until either the user does something; no need for infinite loop here? */
 	while(1) {
-		printf("$ ");
+		printf("\n$$ ");
 
-		/* Attempt to get shell input */
-		fgets(shellInp, shellInpSiz, stdin);
+		/* Attempt to get shell input & handles EOF case */
+		if(fgets(shellInp, shellInpSiz, stdin) == NULL) {
+			exit(0);
+		}
+		
+		/* handle enter for reset case */
+		if(strcmp(shellInp, "\n") ==  0) {
+			continue;
+		}
 		if(shellInp == NULL) {
 			free(shellInp);
 			return NULL;
@@ -44,6 +54,13 @@ char** parseInp(char* shellInp, int* tokenCntAddr) {
 	
 	/* It's token time >:D */
 	token = strtok(shellInp, TOKEN_DELIM);
+	
+	/* handle exit case, weird if i try to do it in readInp function, hmm*/
+	if(strcmp(token, "exit") ==  0) {
+		//account for free-ing?
+		exit(0);
+	}
+
 	while(token != NULL) {
 		*tokenCntAddr = tokenIdx;
 
@@ -80,35 +97,173 @@ char** parseInp(char* shellInp, int* tokenCntAddr) {
 	return tokens;
 }
 
-/* function for input redirection */
-void inputredirect(char **tokens) {
-	__pid_t child_pid, exit_pid;
-	int exit_value;
-
-	child_pid = fork();
-
-	/* child does all the work */
-	if(child_pid == 0) {
-		/*if multiple redirects (<) call program on each one */
-		/* if multiple things to redirect, it ignores the first redirect? */
-		
-	} else { /* parent waits for child to be done*/
-		exit_pid = wait(&exit_value);
-	}	
+void inputredirect(char **tokens, int index, int *fd) {
+	*fd = open(tokens[index+1] , O_RDONLY); 
+	if(*fd == -1) {
+		perror("bash");
+		exit(1); //rather, (maybe it's fine) //set signal to break out of out of the infinite while loop;?
+	}
+	if(dup2(*fd, STDIN_FILENO) == -1) {
+		perror("dup2");
+	}
+	
+	//return *fd;
 }
 
+void outputredirect(char **tokens, int index, int *fd, int isAppend) {
+	if (isAppend == 1) {
+		*fd = open(tokens[index+1], O_WRONLY | O_APPEND);
+		if(*fd == -1) {
+			perror("bash");
+			exit(1); //rather, (maybe it's fine) //set signal to break out of out of the infinite while loop;?
+		}
+
+	}
+	else {
+		*fd = open(tokens[index+1], O_WRONLY | O_TRUNC);
+		if(*fd == -1) {
+			perror("bash");
+			exit(0); //rather, (maybe it's fine) //set signal to break out of out of the infinite while loop;?
+		}
+		
+	}
+
+	dup2(*fd, STDOUT_FILENO);
+	if(dup2(*fd, STDOUT_FILENO) == -1) {
+		perror("dup2");
+	}
+	//return *fd;
+
+}
+
+/* function for I/O redirection */
+void IOredirect(char **tokens, int symIdx, int tknCtr, int isAppend) {
+		__pid_t childpid, exitpid;
+	int exitval;
+	int fd;
+	char **nonsymbol = malloc(sizeof(char*) * COMMAND_LIMIT); //remember to free
+
+	childpid = fork();
+	if(childpid < 0) {
+		perror("fork");
+		exit(1);
+	}
+	/* child does all the work */
+	if(childpid == 0) {	
+		//subset tokens: eh no func. needed i feel like
+		int j = 0;
+		for (int i = 0; i <= tknCtr; i++) { //don't start at 0 start at ; //go till first symbol? or start counter where we left off
+			while(strcmp(tokens[i], "<") == 0 || strcmp(tokens[i], ">") == 0 || strcmp(tokens[i], ">>") == 0) {
+				/* input redirect */
+				if(strcmp(tokens[i], "<") == 0) {
+					inputredirect(tokens,i, &fd);
+				}
+
+				/*output redirect*/
+				if(strcmp(tokens[i], ">") == 0 || strcmp(tokens[i], ">>") == 0) {
+					outputredirect(tokens, i, &fd, isAppend);
+				}
+
+				i+=2;
+
+				if(i > tknCtr) {
+					break;
+				}
+			}
+
+			if(i > tknCtr) {
+				break;
+			}
+
+			nonsymbol[j] = strdup(tokens[i]); //filling in nonsymbol[i] you buffon
+			j++;
+		}
+		
+		//rlly wanna call exec on ALL previous things all things before the symbol (symbol)
+		execvp(nonsymbol[0], nonsymbol); 
+		/*fork bomb protection*/
+		perror("shell");
+		exit(1);
+		
+		
+	} else { /* parent waits for child to be done, then cleans up?*/
+		exitpid = wait(&exitval);
+		//free nonsymbol & close open file descriptors
+	}
+}
+
+
+/* piping functionality */
+/* intermediate pipes will have to dup twice, we don't want the same dup set up (ouptut for one pipe because input for other pipe (2 closes too))*/
+void piping(char ** tokens, int tknCtrPreSym, int offset, int tknCtr) {
+	int pipefd[2];
+	__pid_t child1pid, child2pid, exitpid;
+	int exitval;
+	int fd;
+	char **presym = malloc(sizeof(char*) * COMMAND_LIMIT); //remember to free
+	char **postsym = malloc(sizeof(char*) * COMMAND_LIMIT); //remember to free
+
+	/* parsing, hopefully working */
+	// for (int i = 0; i < tknCtrPreSym; i++) {
+	// 	presym[i] = tokens[i];
+	// }
+	// int j = 0;
+	// for (int b = offset + 1; b <= tknCtr; b++) {
+	// 	postsym[j] = tokens[b];
+	// 	j++;
+	// }
+
+	presym[0] = strdup("ls");
+	postsym[0] = strdup("sort");
+	postsym[1] = strdup("-r");
+ 	/* pipe sys call */
+	pipe(pipefd);
+
+	/* THIS IS BROKEN*/
+	/* fork */
+	child1pid = fork(); //error check
+		/* in child, dup, exec */
+		if(child1pid == 0){
+			write(1, "inhere", 7);
+			close(pipefd[0]); 
+			dup2(pipefd[1], 1);
+			execvp(presym[0], presym);
+		} else { /*the parent should wait, and fork*/
+			//wait here?
+			child2pid = fork();
+			if(child2pid == 0) { /*child 2 will dup,exec*/
+			//close
+				write(1, "inhere", 7);
+				close(pipefd[1]); 	
+				dup2(pipefd[0], 0);
+				execvp(postsym[0], postsym);
+			} else { /*the parent still waits x2*/
+				//or wait twice here
+				wait(NULL);
+				wait(NULL);
+			}
+		}
+}
 
 
 /* basic functionality */
 void basicfunc(char **tokens) {
-			__pid_t child_pid, exit_pid;
+	__pid_t child_pid, exit_pid;
 	int exit_value;
 
 	child_pid = fork();
+	//error check
+	if(child_pid < 0) {
+		perror("fork");
+		exit(1);
+	}
 
 	/* child does all the work */
 	if(child_pid == 0) {
 		execvp(tokens[0], tokens);
+		/*fork bomb protection*/
+		perror("shell");
+		exit(1);
 		
 	} else { /* parent waits for child to be done*/
 		exit_pid = wait(&exit_value);
@@ -117,58 +272,77 @@ void basicfunc(char **tokens) {
 
 
 int main(int argc, char *argv[]) {
-	char* shellInp = readInp();
-	int tokenCnt = 0;
-	char** tokens = parseInp(shellInp, &tokenCnt);
-	int i;
+	while(1) {
+		char* shellInp = readInp();
+		int tokenCnt = 0;
+		char** tokens = parseInp(shellInp, &tokenCnt);
+		int i;
+		int offset;
+		int tknCntPreSym = 0;		
 
-		basicfunc(tokens);
+		
+		printf("%d\n", tokenCnt);
+		for(i = 0; i <= tokenCnt; i++) { /*this loop feels useless, just set flags in parsefunc so we know what ops to do!?*/
+						
+			if (strcmp(tokens[i], "<") == 0) {
+				IOredirect(tokens, i, tokenCnt, 0);
+				continue;
+			}
 
-	for(i = 0; i <= tokenCnt; i++) {
-		//if token == "|"  -> do thing with all previous tokens since last thing done?
-		//if token == ">>" ->
-		//if token == ">"  ->
-		//if token == "<"  ->
-		//exec the shell again
-														//printf("%s\n", tokens[i]);
+			if (strcmp(tokens[i], ">") == 0) {
+				printf("output redirect\n");
+				IOredirect(tokens, i, tokenCnt, 0);
+				continue;
+			}
+			
+			if (strcmp(tokens[i], ">>") == 0) {
+				printf("appended output redirect\n");
+				IOredirect(tokens, i, tokenCnt, 1);
+				continue;
+			}
+
+			if(strcmp(tokens[i], "|") == 0) {
+				printf("piping");
+				piping(tokens,tknCntPreSym, i, tokenCnt);
+			}
+			// 	// for (int a= 0; a < tknCntPreSym; a++) {
+			// 	// 	printf("\n%d\n", a);
+			// 	// }
+			// 	// int j = 0;
+			// 	// offset = i;
+			// 	// for (int b = offset + 1; b <= tokenCnt; b++) {
+			// 	// 	printf("\n%d\n", j);
+			// 	// 	printf("\n%d\n", b);
+			// 	// 	j++;
+			// 	// }
+			// 	/* parsing, hopefully working */
+			// 	// for (int i = 0; i < tknCntPreSym; i++) {
+			// 	// 	printf("\n%d\n",i);
+			// 	// }
+			// 	// int j = 0;
+			// 	// int offset = i;
+			// 	// for (int b = offset + 1; b <= tokenCnt; b++) {
+			// 	// 	printf("\n%d\n",j);
+			// 	// 	printf("\n%d\n",b);
+			// 	// 	j++;
+			// 	// }
+
+			// 	tknCntPreSym = 0;
+			// 	continue;
+			// }	
+
+			//tknCntPreSym++;
+		}
+
+		// IOredirect(tokens, i, tokenCnt, 0); /* < */
+		// IOredirect(tokens, i, tokenCnt, 0); /* > */
+		//IOredirect(tokens, i, tokenCnt, 1); /* >> */
+
+		//basicfunc(tokens); /* only run if there's no symbols */
+
+		
 	}	
-
-	/* function for error checking */
-
-	/* function for tokenizing? */
-
-	/* infinite loop b/cuz the shell is always listening (1984 style)*/
-	//while(1){
-	//	/* print statement for our shell */
-	//	printf("This is our shell: ");
-
-	//	/* get what they give us using fgets & */
-	//	char shellinput[COMMAND_LIMIT]; 
-	//	fgets(shellinput, COMMAND_LIMIT, stdin); //TODO: error check needed? or check that COMMAND_LIMIT is not exceeed
-
-	//	/* replace \n character of input with null byte */
-	//	int cmdlen = strlen(shellinput);
-	//	shellinput[cmdlen-1] = '\0';
-
-	//	/* exit if desired */
-	//	if(strcmp(shellinput, "exit") == 0 || feof(stdin)) {
-	//		exit(0);
-	//	}
-
-	//	/* let's parse with tokenize */
-	//	// while(strtok()) {
-	//	// 	printf("in dev...");
-	//	// }
-	//}
-
-
-	///* Fork, exec, wait, dup */
-
-	///* What we pass to exec() */
-
-	///* use strtok() to parse argv*/
-	///* First argv is the program */
-	///* The rest are command line args to pass to the programs*/
 	return 0;
-
 }
+
+
